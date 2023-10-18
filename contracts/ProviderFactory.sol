25a5d157c140546480ddf48818d7e0c00f702dd2
snakelu <luyuanheng@whitematrix.io>
2023-10-18 09:34:50 +0800

    fix: get margin info list out of index

--- contracts/ProviderFactory.sol
+++ contracts/ProviderFactory.sol
@@ -261,7 +261,7 @@ contract Provider is IProvider, ReentrancyGuard {
         ret.last_challenge_time = last_challenge_time;
         ret.last_margin_time = last_margin_time;
         ret.margin_size = margin_size;
-        uint margin_count = margin_size > 20 ? 20 : margin_size;
+        uint margin_count = margin_size > 10 ? 10 : margin_size;
         marginViewInfo[] memory margin_view_infos = new marginViewInfo[](margin_count);
         for(uint256 i = 0; i < margin_count; i++) {
             margin_view_infos[i] = marginViewInfo(margin_infos[i].margin_amount, margin_infos[i].withdrawn,
@@ -278,7 +278,7 @@ contract Provider is IProvider, ReentrancyGuard {
         uint margin_count = from + size - 1 <= margin_size ? size : margin_size - from + 1;
 
         marginViewInfo[] memory margin_view_infos = new marginViewInfo[](margin_count);
-        for(uint256 i = from - 1; i < from + size - 1; i++) {
+        for(uint256 i = from - 1; i < from + margin_count - 1; i++) {
             margin_view_infos[i] = marginViewInfo(margin_infos[i].margin_amount, margin_infos[i].withdrawn,
                 margin_infos[i].margin_time, margin_infos[i].margin_lock_time, getRemainMarginAmount(i));
         }
